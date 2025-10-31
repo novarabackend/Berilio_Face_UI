051e3eae42c21a4046d5eae9189d8e5645d86772
yava9090 <yava9090@users.noreply.github.com>
2025-10-31 15:49:17 -0500

    Serve navigation locally instead of API calls

---
+++ apps/admin-dashboard/src/app/core/navigation/navigation.data.ts
@@ -0,0 +1,13 @@
+import {
+    compactNavigation,
+    defaultNavigation,
+    futuristicNavigation,
+    horizontalNavigation,
+} from 'app/mock-api/common/navigation/data';
+
+export const navigationData = {
+    default: defaultNavigation,
+    compact: compactNavigation,
+    futuristic: futuristicNavigation,
+    horizontal: horizontalNavigation,
+};
--- apps/admin-dashboard/src/app/core/navigation/navigation.service.ts
+++ apps/admin-dashboard/src/app/core/navigation/navigation.service.ts
@@ -1,11 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Navigation } from 'app/core/navigation/navigation.types';
-import { Observable, ReplaySubject, tap } from 'rxjs';
+import { navigationData } from './navigation.data';
+import { Observable, of, ReplaySubject, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class NavigationService {
-    private _httpClient = inject(HttpClient);
     private _navigation: ReplaySubject<Navigation> =
         new ReplaySubject<Navigation>(1);
 
@@ -28,7 +27,7 @@ export class NavigationService {
      * Get all navigation data
      */
     get(): Observable<Navigation> {
-        return this._httpClient.get<Navigation>('api/common/navigation').pipe(
+        return of(navigationData).pipe(
             tap((navigation) => {
                 this._navigation.next(navigation);
             })
